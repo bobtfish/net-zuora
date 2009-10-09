c53397d4a4c2a59533523c6146d54ade20004492
Tomas Doran (t0m) <t0m@state51.co.uk>
2009-10-09 11:28:53 +0100

    Docs

--- lib/Net/Zuora/Object.pm
+++ lib/Net/Zuora/Object.pm
@@ -13,15 +13,46 @@ sub create {
     Carp::confess("Object $self already exists in Zuora system")
         if $self->_created;
     my $res = $self->_api->_do_create($self);
+    # Force write to ro attribute via MOP.
     $self->meta->get_attribute('_created')->set_value($self, 1);
     return $res;
+}
+
+sub update {
+    my ($self) = @_;
+    Carp::confess("Object $self does not exist in Zuora system")
+        unless $self->_created;
+    $self->_api->_do_update($self);
 }
 
 1;
 
 =head1 NAME
 
-Net::Zuora::Object - Moose role representing an object in the Zuora system
+Net::Zuora::ZObject - Moose role representing an object in the Zuora system
+
+=head1 DESCRIPTION
+
+This is a base L<Moose::Role|role> which all objects that are represented
+in the Zuora API consume.
+
+=head1 METHODS
+
+=head2 create
+
+Creates an object which does not exist already in the Zuora's API.
+
+Throws an exception if the object could not be created for any reason.
+
+=head2 update
+
+Pushes any local updates to this object back to Zuora's API.
+
+Throws an exception if the object could not be updated for any reason.
+
+=head1 ZUORA DOCS
+
+L<http://apidocs.developer.zuora.com/index.php/ZObject>
 
 =head1 AUTHOR
 
